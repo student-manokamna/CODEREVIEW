b12432bc1b6845906c0196ee77101b045eafc990
student-manokamna <aroramoney473@gmail.com>
2026-01-06 02:56:20 +0530

    Fix: fix polar webhook module error

--- app/api/webhooks/polar/route.ts
+++ app/api/webhooks/polar/route.ts
@@ -1,5 +1,5 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function POST(request:Request) {
-//   return NextResponse.json({received:true})
-// }
+export async function POST(request: Request) {
+    return NextResponse.json({ received: true })
+}
