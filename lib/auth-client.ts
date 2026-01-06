66a16fe07351761a0a39e607776df744dcc234d5
student-manokamna <aroramoney473@gmail.com>
2026-01-06 23:12:21 +0530

    Fix: remove baseURL property to resolve localhost connection refused

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -2,6 +2,5 @@ import { polarClient } from "@polar-sh/better-auth"
 import { createAuthClient } from "better-auth/react"
 export const { signIn, signUp, useSession, signOut, customer, checkout } = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NEXT_PUBLIC_APP_BASE_URL,
     plugins: [polarClient()]
 })
