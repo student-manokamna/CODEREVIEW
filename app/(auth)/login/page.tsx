a8c3ab650632c1046460c22881e0ffb99828ce6d
student-manokamna <aroramoney473@gmail.com>
2026-01-06 22:42:34 +0530

    Fix: force dynamic rendering on login page to resolve build error

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,15 +1,17 @@
 import LoginUI from '@/module/auth/components/login-ui'
 import { requireunAuth } from '@/module/utils/auth-utils'
 import React from 'react'
+export const dynamic = "force-dynamic";
 
-const LoginPage  = async () => {
-    await requireunAuth();
+const LoginPage = async () => {
+
+  await requireunAuth();
   return (
     <div>
-      <LoginUI/>
+      <LoginUI />
     </div>
   )
 }
 
-export default LoginPage 
+export default LoginPage
 
